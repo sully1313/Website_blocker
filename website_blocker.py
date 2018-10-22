af674d1042fa47a595579482d559d59495ea379a
Matt Sullivan <mattsullivan@Matts-MacBook-Pro.local>
2018-10-22 10:29:08 -0700

    wip

--- website_blocker.py
+++ website_blocker.py
@@ -9,6 +9,13 @@ website_list = ["www.facebook.com", "facebook.com"]
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() and dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print("...Working hours")
+        with open(hosts_path, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write("%s %s\n"% (redirect, website))
     else:
         print("...Fun Hours")
     time.sleep(5)
