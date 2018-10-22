67edb607a397a75e52e14ad225803ea82435b4e0
Matt Sullivan <mattsullivan@Matts-MacBook-Pro.local>
2018-10-22 11:02:38 -0700

    implemented website blocker which rewrites the hosts file based on the time of day

--- website_blocker.py
+++ website_blocker.py
@@ -9,7 +9,7 @@ website_list = ["www.facebook.com", "facebook.com"]
 
 
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 15) < dt.now() and dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() and dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print("...Working hours")
         with open(hosts_path, 'r+') as file:
             content = file.read()
